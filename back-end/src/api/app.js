ed6a8619b647a662a373ee6b9562e5865d544233
marqsbruno <marqsbruno@gmail.com>
2022-09-27 19:38:01 -0300

    feat:

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -10,6 +10,8 @@ const errorHandler = require('./middlewares/error');
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static('public'));
  
 app.use('/', loginRouter);
 app.use('/', registerRouter);
